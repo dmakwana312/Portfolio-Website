351c0db5a95ef8549ffe20c8e83e5eabff7c6848
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2019-10-20 16:54:54 +0100

    Projects Section Created via JS and XML File

---
+++ scripts/js/projectSection.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    showProjects();
+});
+
+function showProjects() {
+    $.ajax({
+        type: "GET",
+        url: "./projects.xml",
+        dataType: "xml",
+
+        error: function(e) {
+            alert("An Error Occured While Processing XML File");
+        },
+        success: function(response) {
+
+
+            $(response).find("project").each(function() {
+                var xml = $(this);
+                var projectLink = '<div class="col-md-3 project-info"><a href="' + xml.find('github').text() + '"><p class="project-title">' + xml.find('title').text() + '</p><img src="img/project-img/' + xml.find('image').text() + '.png" alt="Project"></a><a href="' + xml.find('github').text() + '" class="btn btn-dark"><i class="fab fa-github"></i>GitHub</a></div>';
+
+                // document.getElementById('project-links').append(projectLink);
+                document.getElementById('project-links').innerHTML += projectLink;
+                console.log(projectLink);
+            })
+        }
+    });
+}
