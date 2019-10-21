d1b3427d77ec44a0e8aa1f07a0fe545925a8f80b
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2019-10-21 16:17:18 +0100

    console.log() removed from projectSection.js

--- scripts/js/projectSection.js
+++ scripts/js/projectSection.js
@@ -17,10 +17,8 @@ function showProjects() {
             $(response).find("project").each(function() {
                 var xml = $(this);
                 var projectLink = '<div class="col-md-3 project-info"><a href="' + xml.find('github').text() + '"><p class="project-title">' + xml.find('title').text() + '</p><img src="img/project-img/' + xml.find('image').text() + '.png" alt="Project"></a><a href="' + xml.find('github').text() + '" class="btn btn-dark"><i class="fab fa-github"></i>GitHub</a></div>';
-
-                // document.getElementById('project-links').append(projectLink);
                 document.getElementById('project-links').innerHTML += projectLink;
-                console.log(projectLink);
+
             })
         }
     });
