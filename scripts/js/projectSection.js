3661ab96248510667b8ad4448fc777959ccc6d65
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2019-10-22 20:56:18 +0100

    Button To Project Page Created

--- scripts/js/projectSection.js
+++ scripts/js/projectSection.js
@@ -16,7 +16,7 @@ function showProjects() {
 
             $(response).find("project").each(function() {
                 var xml = $(this);
-                var projectLink = '<div class="col-md-3 project-info"><a href="' + xml.find('github').text() + '" target="_blank"><p class="project-title">' + xml.find('title').text() + '</p><img src="img/project-img/' + xml.find('image').text() + '.png" alt="Project"></a><a href="' + xml.find('github').text() + '" class="btn btn-dark" target="_blank"><i class="fab fa-github"></i>GitHub</a></div>';
+                var projectLink = '<div class="col-md-4 project-info"><a href="' + xml.find('github').text() + '" target="_blank"><p class="project-title">' + xml.find('title').text() + '</p><img src="img/project-img/' + xml.find('image').text() + '.png" alt="Project"></a><a class= "btn btn-light read-more" href ="http://127.0.0.1:5500/project.html?projectID=' + xml.find('id').text() + '"> Read More</a><a href="' + xml.find('github').text() + '" class="btn btn-dark" target="_blank"><i class="fab fa-github"></i>GitHub</a></div>';
                 document.getElementById('project-links').innerHTML += projectLink;
 
             })
