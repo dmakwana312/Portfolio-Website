3ce575daa3023e533b748a0817d3e89fba38d833
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2019-10-23 16:55:06 +0100

    Project Page Links Changed

--- scripts/js/generateProjectPage.js
+++ scripts/js/generateProjectPage.js
@@ -26,15 +26,15 @@ function showProjects() {
         dataType: "xml",
 
         error: function(e) {
-            alert("An Error Occured While Processing XML File");
+            alert("An Error Occured While Processing XML File\nRedirected to Home Page");
+            location.replace("https://dmakwana312.github.io")
         },
         success: function(response) {
-
+            var contentAdded = false;
 
             $(response).find('project[id="' + projectID + '"]').each(function() {
+                contentAdded = true;
                 var xml = $(this);
-
-
                 document.getElementById('project-title').innerHTML += '<h2>' + xml.find('title').text() + '</h2>';
                 document.getElementById('project-img').innerHTML += '<img class="center" src="img/project-img/' + xml.find('image').text() + '.png">'
                 document.getElementById('more-info').innerHTML += '<p>' + xml.find('about').text() + '</p>';
@@ -45,7 +45,10 @@ function showProjects() {
 
 
             })
-
+            if (!contentAdded) {
+                alert("Project Not Found\nRedirecting To Projects Library");
+                location.replace("https://dmakwana312.github.io/index.html#projects")
+            }
 
         }
 
